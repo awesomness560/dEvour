049aabe24e44cb947cdbc6cff2429c4236edfddf
charz185 <czabelski@gmail.com>
2025-06-13 13:43:19 -0400

    fixes and simplificiation + ordering of objects

--- scripts/pellet_spawner.gd
+++ scripts/pellet_spawner.gd
@@ -1,13 +1,28 @@
 extends Node2D
 @export var pellet:PackedScene
 func _ready() -> void:
-	for i in 100:
-		var f = pellet.instantiate() as Food
-		add_child(f)
-		randomize()
-		var x1 = randi_range(Global.player.global_position.x - 3000,Global.player.global_position.x - 200 )
-		var x2 = randi_range(Global.player.global_position.x + 200,Global.player.global_position.x + 3000 )
-		var y1 = randi_range(Global.player.global_position.y - 3000,Global.player.global_position.y - 200 )
-		var y2 = randi_range(Global.player.global_position.y + 200,Global.player.global_position.y + 3000 )
-		f.global_position.x = randi_range(x1, x2)
-		f.global_position.y = randi_range(y1, y2)
+	spawn(false)
+
+func spawn(usePlayer) -> void:
+	if (usePlayer):
+		for i in 100:
+			var f = pellet.instantiate() as Food
+			add_child(f)
+			randomize()
+			var x1 = randi_range(Global.player.global_position.x - 3000,Global.player.global_position.x - 200 )
+			var x2 = randi_range(Global.player.global_position.x + 200,Global.player.global_position.x + 3000 )
+			var y1 = randi_range(Global.player.global_position.y - 3000,Global.player.global_position.y - 200 )
+			var y2 = randi_range(Global.player.global_position.y + 200,Global.player.global_position.y + 3000 )
+			f.global_position.x = randi_range(x1, x2)
+			f.global_position.y = randi_range(y1, y2)
+	else:
+		for i in 100:
+			var f = pellet.instantiate() as Food
+			add_child(f)
+			randomize()
+			var x1 = randi_range(0 - 3000,0- 200 )
+			var x2 = randi_range(0+ 200,0+ 3000 )
+			var y1 = randi_range(0- 3000,0 - 200 )
+			var y2 = randi_range(0+ 200,0 + 3000 )
+			f.global_position.x = randi_range(x1, x2)
+			f.global_position.y = randi_range(y1, y2)
